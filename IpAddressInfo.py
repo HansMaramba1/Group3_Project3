42d6f9b0b096a5d037f5c0f62eb2341245b684e2
HansMaramba1 <hansmaramba@yahoo.com>
2022-11-25 22:18:06 +0800

    Update IpAddressInfo.py

--- IpAddressInfo.py
+++ IpAddressInfo.py
@@ -1,8 +1,7 @@
-@@ -0,0 +1,7 @@
 import requests
 from requests import get
 
 ip = get('https://api.ipify.org').content.decode('utf8')
 response = requests.get("http://ip-api.com/json/?fields=66846719&lang=en").json()
 
-print("query: "+ip+"\ncountry: "+response["country"]+"\ncountryCode: "+str(response["countryCode"])+"\nregionName: "+response["regionName"]+"\ncity: "+response["city"]+"\nCurrency: "+str(response["currency"])+"\nlatitude: "+str(response["lat"])+"\nlongitude"+str(response["lon"])+"\nasn: "+response["as"]+"\nisp: "+response["isp"])
+print("Ip Address: "+ip+"\nCountry: "+response["country"]+"\nCurrency: "+response["currency"]+"\nCountry Code: "+str(response["countryCode"])+"\nRegion Name: "+response["regionName"]+"\nCity: "+response["city"]+"\nLatitude: "+str(response["lat"])+"\nLongitude"+str(response["lon"])+"\nAsn: "+response["as"]+"\nIsp: "+response["isp"])
